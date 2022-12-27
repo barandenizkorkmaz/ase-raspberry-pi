a0ff2f8e595a5c2ae521674d17984c31e11100aa
ge28vob <deniz.korkmaz@tum.de>
2022-12-27 21:49:27 +0100

    Update rfid.py

--- scripts/rfid.py
+++ scripts/rfid.py
@@ -1,9 +1,11 @@
+import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 reader = SimpleMFRC522()
 
 try:
     while True:
         id, text = reader.read()
+        print(f'Id: {id}\tText: {text}')
 except KeyboardInterrupt:
     GPIO.cleanup()
     raise
