b07d9462d1851f8f042452eddfc8c1b9a2502a4c
Baran Deniz Korkmaz <deniz.korkmaz@tum.de>
2022-12-30 22:06:20 +0000

    Box logic implemented and tested

--- main.py
+++ main.py
@@ -3,20 +3,28 @@ import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 import json
 import time
+import requests
+from requests import Session
 
-reader = SimpleMFRC522()
+def initializeBox():
+    f = open(CONFIG_PATH)
+    return json.load(f)
+
+CONFIG_PATH = 'config.json'
+BOX_DATA = initializeBox()
+BOX_ID = BOX_DATA['id']
+BOX_NAME = BOX_DATA['name']
+BOX_ADDRESS = BOX_DATA['address']
+BOX_RASPBERRY_ID = BOX_DATA['raspberryId']
+
+HOST_NAME = '192.168.68.123'
+PORT = 8083
+HOST_URL = f'http://{HOST_NAME}:{str(PORT)}'
 
 GREEN_PIN = 38
 RED_PIN= 37
 RFID_READER_PIN = 11 # ???
 PHOTORESISTOR_READER_PIN = 40
-
-HOST_NAME = 'localhost'
-PORT = 8083
-HOST_URL = f'http://{HOST_NAME}:{str(PORT)}'
-
-
-USERS_PATH = 'users.json'
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
@@ -26,6 +34,9 @@ GPIO.setup(RED_PIN, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(RFID_READER_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(PHOTORESISTOR_READER_PIN,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setwarnings(True)
+
+reader = SimpleMFRC522()
+session = requests.Session()
 
 def light_led(color, seconds=3):
     GPIO.output(color, GPIO.HIGH)
@@ -38,27 +49,46 @@ def blink_led(color, seconds=0.1):
     GPIO.output(color, GPIO.LOW)
     sleep(seconds)
 
-def authenticate(userId):
-    f = open(USERS_PATH)
-    credentials = json.load(f)
-    for item in credentials:
-        if item["id"] == userId:
-            print(f"Found Match for ID {item['id']}")
-            return True
-    return False
+def boxUnlock(rfId):
+    global session
+    params = {
+        "id": BOX_ID
+    }
+    content = {
+        "rfid": rfId
+    }
+    print("Sending box unlock request to the server.")
+    result = session.post(
+        url=f'{HOST_URL}/box/unlock/{BOX_ID}',
+        params=params,
+        headers='',
+        json=content
+    
+    )
+    print("Received the following box unlock response from the server.")
+    print(result)
+    print(f"Status Code: {result.status_code}")
+    return result.status_code == 200
 
-def httpRequest(method, url, params, headers='', content='', auth=''):
-    if method == 'GET':
-        result = session.get(url, params=params)
-        return result
-    elif method == 'POST':
-        if auth == '':
-            result = session.post(url, params=params, headers=headers, json=content)
-        else:
-            result = session.post(url, params=params, headers=headers, auth=auth)
-        return result
-    else:
-        raise ValueError('Method Not Found')
+def boxLock(rfId):
+    global session
+    params = {
+        "id": BOX_ID
+    }
+    content = {
+        "rfid": rfId
+    }
+    print("Sending box lock request to the server.")
+    result = session.post(
+        url=f'{HOST_URL}/box/lock/{BOX_ID}',
+        params=params,
+        headers='',
+        json=content
+    
+    )
+    print("Received the following box lock response from the server.")
+    print(result)
+    print(f"Status Code: {result.status_code}")
 
 try:
     while True:
@@ -78,23 +108,24 @@ try:
             Restart the device (Output)
             Sleep for 1 secs
         """
-        print("### Welcome to ASE Delivery ###\nPlease scan your RFID tag to device.")
-        isOpen = False
-        id, data = reader.read()
-        data = data.strip()
-        print(f"The tag has been successfully scanned.\nID: {id}\tData: {data}")
-        if(authenticate(data.strip())):
+        print("### Welcome to ASE Delivery ###")
+        print(f"### Box Information ### \nId: {BOX_ID}\nName: {BOX_NAME}\nAddress: {BOX_ADDRESS}\nRaspberry ID: {BOX_RASPBERRY_ID}")
+        print("Please scan your RFID tag to device.")
+        cardId, rfId = reader.read()
+        rfId = rfId.strip()
+        print(f"The tag has been successfully scanned.\nID: {cardId}\RfId: {rfId}")
+        if(boxUnlock(rfId)):
             light_led(GREEN_PIN)
             now = time.time()
             while not int(str(GPIO.input(PHOTORESISTOR_READER_PIN))):
                 if time.time() > now + 10:
                     blink_led(RED_PIN)
             print('Sending lock request.')
+            boxLock(rfId)
         else:
             light_led(RED_PIN)
         print("Please wait until the device has been restarted")
         sleep(0.5)
-
 except KeyboardInterrupt:
     GPIO.cleanup()
     raise
